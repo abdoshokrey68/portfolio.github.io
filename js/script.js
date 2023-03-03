1d56aab9094a44f4f17b226422677754232880df
abdoshokrey68 <abdoshokrey68@gmail.com>
2023-03-03 03:35:54 +0200

    Upload Latest Updates

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-// gsap.registerPlugin(ScrollTrigger)
-
-// gsap.to('.about_me', {
-// 	x: 100,
-// 	duration: 1,
-// 	scrollTrigger: ".about_me", // start the animation when ".box" enters the viewport (once)
-// })
-
-// AOS.init();
-
 $(document).ready(function () {
-	
+
     var swiper = new Swiper(".mySwiper", {
 		slidesPerView: 4,
+		breakpoints: {  
+			'1000': {
+				slidesPerView: 5,
+				spaceBetween: 40,
+			},
+			'800': {
+				slidesPerView: 4,
+				spaceBetween: 40,
+			},
+			'480': {
+				slidesPerView: 2,
+				spaceBetween: 30,
+			},
+			'300': {
+				slidesPerView: 1,
+				spaceBetween: 20,
+			},
+		},
 		spaceBetween: 30,
 		pagination: {
 			el: ".swiper-pagination",
@@ -24,23 +32,32 @@ $(document).ready(function () {
 		},
 	});
 	
+	// Handle Card Title Slide Down 
 	$(".card_title").click(function () {
-		$(this).children('i').toggleClass('fa-plus')
-		$(this).children('i').toggleClass('fa-minus')
+		let check = 0;
+		$(this).hasClass("active") ? check++ : false;
 		$('.card_title').removeClass("active")
-		$(this).toggleClass("active")
+		$('.card_title i').removeClass("fa-minus")
+		$('.card_title i').addClass("fa-plus")
 		$('.card_drop_down_section').slideUp();
-		$(this).next().slideToggle()
+		if (check === 0)
+		{
+			$(this).children('i').toggleClass('fa-plus')
+			$(this).children('i').toggleClass('fa-minus')
+			$(this).toggleClass("active")
+			$(this).next().slideToggle()
+		}
 	})
 	
+	// Handle Show Projects Name Less Than 25 Letters
 	let my_work__s__title = document.getElementsByClassName("my_work__s__title");
-	
-	$('.my__work_info p').each(function () {
+	$('.my__work_info h6').each(function () {
 		$(this).html($(this).html().length > 25 ? $(this).html().substr(0, 25) + " ..." : $(this).html())
 	})
 	
+
+	// My Work Experiance Section
 	var my__works_count_show = 2;
-	
 	const HandleshowMySingleProject  = function (count) {
 		const single_pro_container = $('.single_pro_container');
 		for (let i = 0; i < single_pro_container.length; i++) {
@@ -79,13 +96,6 @@ $(document).ready(function () {
 		$(".navbar_button_open").delay(400).slideDown(300)
 	})
 
-	// const AutherImagesArray = ["./images/328846155_507202214881574_3942122195554899770_n.jpg", "./images/rtrewq.jpg"];
-	// var count = 0;
-	// setInterval(() => {
-	// 	$("#auther_navbar_image_header").attr("src", AutherImagesArray[count])
-	// 	count >= 1 ? count = 0 : count ++;
-	// }, 1000);
-
 	$(".hire_me_btn").on("click", function () {
 		$(".hire_me_popup_container").toggleClass("d-none")
 	})
@@ -107,5 +117,43 @@ $(document).ready(function () {
 		})
 	})
 
+	const ChangePortfolioTheme = function (class_value) {
+		$("body").toggleClass(class_value)
+		$(".main_paragraph").toggleClass(class_value)
+		$(".lef-navbar").delay(200).toggleClass(class_value)
+		$(".theme_scale_container").toggleClass(class_value)
+		$(this).toggleClass(class_value)
+		$(".toggle_light").toggleClass('text-light')
+		$(".change__mode_container i").delay(300).toggleClass("fa-sun fa-moon")
+		$(".change__mode_container i").toggleClass(class_value)
+	}
+
+	// Change Theme Mode 
+	$(".change__mode_container").on("click", function () {
+		$('body').hasClass('theme') || $('body').hasClass('default_dark') ? localStorage.setItem("theme", "light") : localStorage.setItem('theme', 'dark') ;
+		if ($('body').hasClass("default_dark")) 
+		{		
+			$('.default_dark').toggleClass('default_dark')
+			$(".toggle_light").toggleClass('text-light')
+			$(".change__mode_container i").delay(300).toggleClass("fa-sun fa-moon")
+		} else {
+			ChangePortfolioTheme('theme')
+		}
+
+	})
+
+	const CheckIfThemeIsSavedInLocalStorage = function () {
+		if(localStorage.getItem("theme"))
+		{
+			if (localStorage.getItem('theme') === "dark")
+			{
+				ChangePortfolioTheme('default_dark')
+			}
+		} else {
+			localStorage.setItem('theme', 'light');
+		}
+	}
+
+	CheckIfThemeIsSavedInLocalStorage()
 });
 
